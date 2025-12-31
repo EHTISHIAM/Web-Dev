f6280b77f749a8c77525d6052e8296b03b3392f6
Prince-Ehtisham <the.prince.ehtisham@email.com>
2025-12-30 22:58:27 -0800

    WEB DEV Courses

---
+++ JavaScript/Varaible.js
@@ -0,0 +1,16 @@
+console.log("Prince");
+// The console.log is used to print the message on the console and the single and double comma is used and semicolon is used for the best practice or your choice 
+console.log("I Love Js");
+//Varaible store the value like the name,age,price
+Name="Ehtisham";
+console.log(Name);
+age = 24;
+console.log(age);
+Price=454.45;
+console.log(Price);
+x=null; // there is a value in it which is empty basically 
+y=undefined;// there is no clue what is in it
+// ALso Boolean Value in it like true or false 
+// Js is a dynamically type any type of a varaible can store any type of a value like the above name 
+Name = 45;
+console.log(Name);
