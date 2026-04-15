0b2af606aa7814e8b40084c11a3cf1d37764c760
EHTISHAM <the.prince.ehtisham@gmail.com>
2026-04-15 13:58:21 +0500

    Added a new concept of the child class

--- JavaScript/Inheritance.js
+++ JavaScript/Inheritance.js
@@ -12,3 +12,25 @@ let obj = new child();
 console.log(child);
 
 console.log(obj.hello()); //accessing the data from the parent using the extend 
+
+
+class human {
+    eat () {
+        console.log("Eats");
+    }
+    sleep () {
+        console.log("Sleep");
+    }
+}
+
+class engineer extends human {
+    work () {
+        console.log("Code");
+    }
+}
+
+class doctor extends human {
+    work () {
+        console.log("Doctor");
+    }
+}
