63a54fd91901e55f129d6681c4890fc372448941
EHTISHAM <the.prince.ehtisham@gmail.com>
2026-04-15 14:04:03 +0500

    Added the objects to initilize the class

--- JavaScript/Inheritance.js
+++ JavaScript/Inheritance.js
@@ -34,3 +34,14 @@ class doctor extends human {
         console.log("Doctor");
     }
 }
+
+let object1 = new human();
+console.log(object1.eat());
+
+let object2 = new engineer();
+console.log(object2.eat(),object2.work()); //the eat is from the human parent class
+
+let object3= new doctor();
+console.log(object3.eats(),object3.work()); 
+
+//if the parent and class have the function with samee name then the function of child class will be used 
