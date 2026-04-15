5c9da91b7e2401fb21d069e1d2a0f65114d282b7
EHTISHIAM <ehtishiam@gmail.com>
2026-04-15 13:48:58 +0500

    Make the class

--- JavaScript/Inheritance.js
+++ JavaScript/Inheritance.js
@@ -1,1 +1,14 @@
 //Inheritance means passing downs the methods and properites from the parent class to child class and a key word is used to inherit the properties and method which is extend
+class parent {
+    hello (){
+        console.log("I am the parent ");
+    }
+}
+
+class child extends parent {} 
+
+let obj = new child();
+
+console.log(child);
+
+console.log(obj.hello()); //accessing the data from the parent using the extend 
