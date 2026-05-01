fde6f8668334f566c5d7ffbefbc9d1341490ca6e
EHTISHAM <the.prince.ehtisham@gmail.com>
2026-05-01 18:36:34 +0500

    Added a js file

---
+++ JavaScript/Promise-Chain.js
@@ -0,0 +1,39 @@
+function getData(ID) {
+    return new Promise ( (resolve , reject )=>{
+        setTimeout(()=>{
+            console.log("Data is ",ID);
+            resolve("Success")
+        },3000);
+    });
+}
+
+getData(1).then((res)=>{
+    return getData(2);
+}).then((res)=>{  //Promise Chain is better than the callback hell 
+    console.log(res);
+})
+
+//Asyn Awaits is more better than the Promises chains 
+
+//Asyn Function always return the promises    async Function myFun(){..........}
+async function HelloFun() {
+    console.log("Say the Hello ");
+}
+console.log(HelloFun());  //Check the property of this function on the console log which returns the same three states of promises 
+
+//Await keyword is for the async function only and awaits pauses the execution of its surrounding async function until the prommise is settled 
+
+//Now I am calling the same function get data again using the await 
+
+async function getAllData () {
+    console.log("Getting Data 3 through the Await ");
+    await getData(3);
+    console.log("Getting Data 4 through the Await");
+    await getData(4);
+    console.log("Getting Data 5 through the Await");
+    await getData(5).then((res)=>{  //uses then along the await 
+        console.log(res , "Await One ");
+    });
+}
+
+getAllData();
