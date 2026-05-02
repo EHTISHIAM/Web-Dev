f8465240e45dd6a0aca578bc80316bba84c39b33
EHTISHAM <the.prince.ehtisham@gmail.com>
2026-05-02 15:48:53 +0500

    added the def of fetch

---
+++ JavaScript/Fetch-API.js
@@ -0,0 +1,4 @@
+//API is basically called as Application Programming Interface which is used to take the data from the another server 
+
+
+//Fetch API provides an interface for fetching the (sending/recieving) resources . It uses the request and response objects 
