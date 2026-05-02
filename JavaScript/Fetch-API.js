8a45b0a8bb7c72a7454be5ac2eabae90ae67ef8b
EHTISHIAM <ehtishiam@gmail.com>
2026-05-02 16:08:36 +0500

    Added the comment

--- JavaScript/Fetch-API.js
+++ JavaScript/Fetch-API.js
@@ -3,11 +3,15 @@
 
 //Fetch API provides an interface for fetching the (sending/recieving) resources . It uses the request and response objects 
 //The fetch method is used to fetch the resources (data) . They return the promises which has three states 
-//let promise = fetch(url,[options])
+//let promise = fetch(url,[options]) when we do not add any particular opotion the fetch is sending the GET request to the server
 
 const URL = "https://dogapi.dog/api-docs/v2/swagger.json"
 
 const getFacts = async () => {
+    console.log("Fetching the Data .........");
     let response =  await fetch(URL);
     console.log(response);
 }
+
+//We are not getting the actual data which is presents on tha API we are getting data in the form of the promises 
+
